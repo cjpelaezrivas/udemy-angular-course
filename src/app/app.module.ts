22dcf5ce9d0b34a7e5b7e4de4deea1233be0cf61
Carlos J. Pelaez <cpelaez@theworkshop.com>
2022-02-17 09:47:20 +0100

    Section 2 - Part 1. Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { AlertWarningComponent } from './alert-warning/alert-warning.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertWarningComponent,
+    AlertSuccessComponent
   ],
   imports: [
     BrowserModule
